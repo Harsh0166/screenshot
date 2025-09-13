2e392521d27aebd1eff8971dec0bd287ddfa8107
Harsh0166 <2005harshkumar@gmail.com>
2025-09-13 22:52:12 +0530

    ff

--- background.js
+++ background.js
@@ -6,9 +6,17 @@ chrome.contextMenus.create({
     contexts: ["all"],
 });
 
-chrome.contextMenus.onClicked.addListener(function(info) {
+chrome.contextMenus.onClicked.addListener(function(info) { 
+    /* info : It contains details about the context menu click, such as:
+    menuItemId → the ID of the menu item clicked ("takeScreenShot" in your case)
+    pageUrl → the URL of the page wher the click happened
+    selectionText → any selected text, if applicable*/
+
     if (info.menuItemId === "takeScreenShot") {
         chrome.tabs.captureVisibleTab({ format: "png" }, function(screenshotUrl) {
+            /* screenshotUrl : It is provided by chrome.tabs.captureVisibleTab.
+            Chrome generates the screenshot and passes a data URL (a base64-encoded image URL) to this parameter.*/
+
             chrome.downloads.download({
                 url: screenshotUrl,
                 filename: "screenshots/screenshots.png"
