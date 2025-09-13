371dbe42c4cc79b87c8ad3126a498ef4c810170e
Harsh0166 <2005harshkumar@gmail.com>
2025-09-13 22:24:32 +0530

    up

--- background.js
+++ background.js
@@ -1,22 +1,19 @@
 console.log("Background service worker started.");
 
 chrome.contextMenus.create({
-    id : "takeScreenShot",
-    title : "ScreenShot",
-    contexts : ["all"],
+    id: "takeScreenShot",
+    title: "ScreenShot",
+    contexts: ["all"],
 });
 
-chrome.contextMenus.onClicked.addListener((info,tab) =>{
-    if(info.menuItemId === "takeScreenShot"){
-        chrome.tabs.captureVisibleTab({format: "png"},(screenshotUrl)=>{
+chrome.contextMenus.onClicked.addListener(function(info) {
+    if (info.menuItemId === "takeScreenShot") {
+        chrome.tabs.captureVisibleTab({ format: "png" }, function(screenshotUrl) {
             chrome.downloads.download({
-                url : screenshotUrl,
-                filename : "screenshots/screenshots.png"
-            })
-            console.log("screenshot taken");}
-        )
-        
+                url: screenshotUrl,
+                filename: "screenshots/screenshots.png"
+            });
+            console.log("screenshot taken");
+        });
     }
-
-
-})
+});
